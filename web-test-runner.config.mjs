1d1e8bd254c5fd48f470cf6ebd53fe7c00000240
Manuel Martin <manuel.martin@gmail.com>
2022-05-05 14:24:04 +0200

    chore: install playwright

--- web-test-runner.config.mjs
+++ web-test-runner.config.mjs
@@ -1,3 +1,5 @@
+import { playwrightLauncher } from '@web/test-runner-playwright';
+
 export default {
   /** test files to run */
   files: 'src/**/*.test.js',
@@ -5,4 +7,10 @@ export default {
   /** resolve bare imports */
   nodeResolve: true,
 
+  /** browsers to run tests on */
+  browsers: [
+    playwrightLauncher({ product: 'chromium' }),
+    playwrightLauncher({ product: 'firefox' }),
+    playwrightLauncher({ product: 'webkit' }),
+  ],
 };
